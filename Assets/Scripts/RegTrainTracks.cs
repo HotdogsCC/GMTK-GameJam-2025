a7c9217d1f5ec897dcb2021da8cdc017aaaa1dc3
Charlie Campbell <chacha9669@hotmail.com>
2025-08-01 13:34:21 +1000

    switching tracks work

--- Assets/Scripts/RegTrainTracks.cs
+++ Assets/Scripts/RegTrainTracks.cs
@@ -4,5 +4,13 @@ using UnityEngine;
 
 public class RegTrainTracks : TrackBase
 {
-    
+    public override Vector3 GetExit1Pos()
+    {
+        return exitTransform1.position;
+    }
+
+    public override Vector3 GetExit2Pos()
+    {
+        return exitTransform2.position;
+    }
 }
